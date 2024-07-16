1bb459597e9648ee35cd1d08b0112a2d9edcbf22
nada-hesham <85525572+NA-MO@users.noreply.github.com>
2024-07-16 16:36:27 +0300

    fix suspense

--- src/components/ImageSearchResults.tsx
+++ src/components/ImageSearchResults.tsx
@@ -1,44 +1,43 @@
-import Link from 'next/link';
-import PaginationButtons from './PaginationButtons';
+import Link from "next/link";
+import PaginationButtons from "./PaginationButtons";
 import { Suspense } from "react";
 
-type results ={
-    results:any;
+type results = {
+  results: any;
 };
-export default function ImageSearchResults({ results }:results) {
+export default function ImageSearchResults({ results }: results) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-
-    <div className='sm:pb-24 pb-40 mt-4'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
-        {results.items.map((result: any) => (
-          <div className='mb-8' key={result.link}>
-            <div className='group'>
-              <Link href={result.image.contextLink}>
-                <img
-                  src={result.link}
-                  alt={result.title}
-                  className='h-60 group-hover:shadow-xl w-full object-contain transition-shadow duration-300'
+      <div className="sm:pb-24 pb-40 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
+          {results.items.map((result: any) => (
+            <div className="mb-8" key={result.link}>
+              <div className="group">
+                <Link href={result.image.contextLink}>
+                  <img
+                    src={result.link}
+                    alt={result.title}
+                    className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow duration-300"
                   />
-              </Link>
-              <Link href={result.image.contextLink}>
-                <h2 className='group-hover:underline truncate text-xl'>
-                  {result.title}
-                </h2>
-              </Link>
-              <Link href={result.image.contextLink}>
-                <p className='group-hover:underline truncate text-gray-600'>
-                  {result.displayLink}
-                </p>
-              </Link>
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <h2 className="group-hover:underline truncate text-xl">
+                    {result.title}
+                  </h2>
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <p className="group-hover:underline truncate text-gray-600">
+                    {result.displayLink}
+                  </p>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="ml-16">
+          <PaginationButtons />
+        </div>
       </div>
-      <div className='ml-16'>
-        <PaginationButtons />
-      </div>
-    </div>
-  </Suspense>
+    </Suspense>
   );
 }

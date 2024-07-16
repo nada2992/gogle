9a4c693caba86e01be8285e80bd2da90c74cb542
nada-hesham <85525572+NA-MO@users.noreply.github.com>
2024-07-16 16:29:32 +0300

    fixxx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import HomeHeader from "@/components/HomeHeader";
 import HomeSearch from "@/components/HomeSearch";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <>
-      <HomeHeader/>
-      <div className='flex flex-col items-center mt-24'>
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
         <Image
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
-          alt='Google Logo'
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+          alt="Google Logo"
           width={300}
           height={100}
           priority
-          style={{ width: 'auto' }}
+          style={{ width: "auto" }}
         />
-      <HomeSearch/>
-      </div>      
-    </>  
+        <HomeSearch />
+      </div>
+    </Suspense>
   );
 }
